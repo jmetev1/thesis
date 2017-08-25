1f5f26802ec2cb372ca6ac0d2efd8ae365a7d7f2
Jmetev1 <jmetevier@gmail.com>
2017-08-25 12:26:47 -0500

    [add] remove everything

--- src/pages/homepage/homepage.ts
+++ src/pages/homepage/homepage.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 import { CatProvider } from '../../app/providers/cat-provider/cat-provider';
-import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion';
+// import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion';
 
 @IonicPage()
 @Component({
@@ -18,52 +18,53 @@ export class Homepage {
 
   constructor(
     public catProvider:CatProvider,
-    private navController: NavController,
-    platform:Platform,
-    private deviceMotion: DeviceMotion,
-  ) {
-    this.loadCats();
-    let ready = false;
-    platform.ready().then(() => {
-      ready = true;
-      // var subscription = deviceMotion.watchAcceleration({frequency:200}).subscribe(acc => {
-      //   if(!this.lastX) {
-      //     this.lastX = acc.x;
-      //     this.lastY = acc.y;
-      //     this.lastZ = acc.z;
-      //     return;
-      //   }
-      //   console.log(acc)
-      //   // let deltaX:number, deltaY:number, deltaZ:number;
-      //   // deltaX = Math.abs(acc.x-this.lastX);
-      //   // deltaY = Math.abs(acc.y-this.lastY);
-      //   // deltaZ = Math.abs(acc.z-this.lastZ);
-
-      //   // if(deltaX + deltaY + deltaZ > 3) {
-      //   //   this.moveCounter++;
-      //   // } else {
-      //   //   this.moveCounter = Math.max(0, --this.moveCounter);
-      //   // }
-      //   // if(this.moveCounter > 2) {
-      //   //   console.log('SHAKE');
-      //   //   this.loadCats();
-      //   //   this.moveCounter=0;
-      //   // }
-      //   // this.lastX = acc.x;
-      //   // this.lastY = acc.y;
-      //   // this.lastZ = acc.z;
-      // });
-    });
-
+    private navController: NavController)
+    {}
+    loadMore() {
+      console.log('load more cats');
+      this.loadCats();
+    }
+    loadCats() {
+      this.catProvider.load().then(result => {
+        this.cats = result;
+      });
+    }
   }
+  //   platform:Platform,
+  //   private deviceMotion: DeviceMotion,
+  // ) {
+  //   this.loadCats();
+  //   let ready = false;
+  //   platform.ready().then(() => {
+  //     ready = true;
+  //     var subscription = deviceMotion.watchAcceleration({frequency:200}).subscribe(acc => {
+  //       if(!this.lastX) {
+  //         this.lastX = acc.x;
+  //         this.lastY = acc.y;
+  //         this.lastZ = acc.z;
+  //         return;
+  //       }
+  //       console.log(acc)
+  //       // let deltaX:number, deltaY:number, deltaZ:number;
+  //       // deltaX = Math.abs(acc.x-this.lastX);
+  //       // deltaY = Math.abs(acc.y-this.lastY);
+  //       // deltaZ = Math.abs(acc.z-this.lastZ);
 
-  loadMore() {
-    console.log('load more cats');
-    this.loadCats();
-  }
-  loadCats() {
-    this.catProvider.load().then(result => {
-      this.cats = result;
-    });
-  }
-}
+  //       // if(deltaX + deltaY + deltaZ > 3) {
+  //       //   this.moveCounter++;
+  //       // } else {
+  //       //   this.moveCounter = Math.max(0, --this.moveCounter);
+  //       // }
+  //       // if(this.moveCounter > 2) {
+  //       //   console.log('SHAKE');
+  //       //   this.loadCats();
+  //       //   this.moveCounter=0;
+  //       // }
+  //       // this.lastX = acc.x;
+  //       // this.lastY = acc.y;
+  //       // this.lastZ = acc.z;
+  //     });
+  //   });
+
+  // }
+
