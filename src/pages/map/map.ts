0c3ec97091101e580e725f6b065582ee476999d3
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-08-31 11:31:56 -0500

    [add]google map

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,6 @@
-import { GoogleMap, GoogleMapsEvent } from '@ionic-native/google-maps';
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { GoogleMap, GoogleMaps, GoogleMapsEvent, LatLng, MarkerOptions, Marker } from '@ionic-native/google-maps';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, Platform } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,46 +8,73 @@ import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
   templateUrl: 'map.html',
 })
 
-export class MapPage {
-  map: GoogleMap;
+export class MapPage implements AfterViewInit {
+  @ViewChild('map') element;
 
-     constructor(public navCtrl: NavController, public platform: Platform) {
-         platform.ready().then(() => {
-             this.loadMap();
-         });
-     }
+  constructor(public navCtrl: NavController, public platform: Platform, public googleMaps: GoogleMaps) {}
+  ngAfterViewInit() {
+    this.platform.ready().then(() => {
+      this.initMap();
+    })
+  }
+  initMap() {
 
-     loadMap(){
+        let map: GoogleMap = this.googleMaps.create(this.element.nativeElement);
 
+        map.one(GoogleMapsEvent.MAP_READY).then((data: any) => {
 
-         this.map = new GoogleMap('map', {
-           'controls': {
-             'compass': true,
-             'myLocationButton': true,
-             'indoorPicker': true,
-           },
-           'gestures': {
-             'scroll': true,
-             'tilt': true,
-             'rotate': true,
-             'zoom': true
-           },
-           'camera': {
-             'target': {
-               'lat': 43.0741904,
-               'lng': -89.3809802
-             },
-             'tilt': 30,
-             'zoom': 15,
-             'bearing': 50
-           }
-         });
+          let coordinates: LatLng = new LatLng(33.6396965, -84.4304574);
 
-         this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
-             console.log('Map is ready!');
-         });
+          let position = {
+            target: coordinates,
+            zoom: 17
+          };
 
-     }
+          map.animateCamera(position);
+
+          let markerOptions: MarkerOptions = {
+            position: coordinates,
+            icon: "assets/images/icons8-Marker-64.png",
+            title: 'Our first POI'
+          };
+
+          const marker = map.addMarker(markerOptions)
+            .then((marker: Marker) => {
+              marker.showInfoWindow();
+          });
+        })
+      }
 }
 
+
+// platform.ready().then(() => {
+//   this.map = new GoogleMap('map', {
+//     'controls': {
+//       'compass': true,
+//       'myLocationButton': true,
+//       'indoorPicker': true,
+//     },
+//     'gestures': {
+//       'scroll': true,
+//       'tilt': true,
+//       'rotate': true,
+//       'zoom': true
+//     },
+//     'camera': {
+//       'target': {
+//         'lat': 43.0741904,
+//         'lng': -89.3809802
+//       },
+//       'tilt': 30,
+//       'zoom': 15,
+//       'bearing': 50
+//     }
+//   });
+
+//   this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+//       console.log('Map is ready!');
+//   });
+
+// });
+// }
 
