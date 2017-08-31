ffefe3312aaadd7f0390e2b6b9a5dad39c70b9a0
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-08-31 14:41:12 -0500

    try 47

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -11,42 +11,25 @@ import { IonicPage, NavController, Platform } from 'ionic-angular';
 export class MapPage {
   @ViewChild('map') element;
   error: any = 'ERROR';
-  constructor(
-    public navCtrl: NavController,
-    public platform: Platform,
-    public googleMaps: GoogleMaps,
-  ) {
-    // ngAfterViewInit() {
+  constructor( public navCtrl: NavController, public platform: Platform, public googleMaps: GoogleMaps,) {
+    const initMap = () => {
+      this.error = 'in initmap';
+      let map: GoogleMap = this.googleMaps.create(this.element.nativeElement);
+      map.one(GoogleMapsEvent.MAP_READY).then((data: any) => {
+        this.error = `line 24`;
+        let coordinates: LatLng = new LatLng(33.6396965, -84.4304574);
+        let position = {
+          target: coordinates,
+          zoom: 17
+        }
+        map.animateCamera(position);
+      }).catch(err => {
+        this.error = `line 42`;
+      })
+    }
       this.platform.ready().then(() => {
         this.error = 'in platform ready';
-        this.initMap();
+        initMap();
       })
-    // }
-  }
-  initMap() {
-    this.error = 'in initmap';
-        let map: GoogleMap = this.googleMaps.create(this.element.nativeElement);
-        map.one(GoogleMapsEvent.MAP_READY).then((data: any) => {
-          this.error = `line 24`;
-          let coordinates: LatLng = new LatLng(33.6396965, -84.4304574);
-          let position = {
-            target: coordinates,
-            zoom: 17
-          }
-          map.animateCamera(position);
-
-          // let markerOptions: MarkerOptions = {
-          //   position: coordinates,
-          //   icon: "assets/images/icons8-Marker-64.png",
-          //   title: 'Our first POI'
-          // };
-
-          // const marker = map.addMarker(markerOptions)
-          //   .then((marker: Marker) => {
-          //     marker.showInfoWindow();
-          // });
-        }).catch(err => {
-          this.error = `line 42`;
-        })
-      }
+    }
 }
