bd216e6de6986bc95088a905386ff88ec49e6e34
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-08-31 14:56:54 -0500

    change routes

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,35 +1,101 @@
-import { GoogleMap, GoogleMaps, GoogleMapsEvent, LatLng, MarkerOptions, Marker } from '@ionic-native/google-maps';
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, Platform } from 'ionic-angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+ } from '@ionic-native/google-maps';
+ import { Component } from "@angular/core/";
+ 
+ @Component({
+   selector: 'page-map',
+   templateUrl: 'map.html'
+ })
+ export class MapPage {
+   map: GoogleMap;
+   mapElement: HTMLElement;
+   constructor(private googleMaps: GoogleMaps) { }
+ 
+   ionViewDidLoad() {
+    this.loadMap();
+   }
+ 
+  loadMap() {
+     this.mapElement = document.getElementById('map');
+ 
+     let mapOptions: GoogleMapOptions = {
+       camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+     };
+ 
+     this.map = this.googleMaps.create(this.mapElement, mapOptions);
+ 
+     // Wait the MAP_READY before using any methods.
+     this.map.one(GoogleMapsEvent.MAP_READY)
+       .then(() => {
+         console.log('Map is ready!');
+ 
+         // Now you can use all methods safely.
+         this.map.addMarker({
+             title: 'Ionic',
+             icon: 'blue',
+             animation: 'DROP',
+             position: {
+               lat: 43.0741904,
+               lng: -89.3809802
+             }
+           })
+           .then(marker => {
+             marker.on(GoogleMapsEvent.MARKER_CLICK)
+               .subscribe(() => {
+                 alert('clicked');
+               });
+           });
+ 
+       });
+   }
+ }
 
-@IonicPage()
-@Component({
-  selector: 'page-map',
-  templateUrl: 'map.html',
-})
+// import { GoogleMap, GoogleMaps, GoogleMapsEvent, LatLng, MarkerOptions, Marker } from '@ionic-native/google-maps';
+// import { AfterViewInit, Component, ViewChild } from '@angular/core';
+// import { IonicPage, NavController, Platform } from 'ionic-angular';
 
-export class MapPage {
-  @ViewChild('map') element;
-  error: any = 'ERROR';
-  constructor( public navCtrl: NavController, public platform: Platform, public googleMaps: GoogleMaps,) {
-    const initMap = () => {
-      this.error = 'in initmap';
-      let map: GoogleMap = this.googleMaps.create(this.element.nativeElement);
-      map.one(GoogleMapsEvent.MAP_READY).then((data: any) => {
-        this.error = `line 24`;
-        let coordinates: LatLng = new LatLng(33.6396965, -84.4304574);
-        let position = {
-          target: coordinates,
-          zoom: 17
-        }
-        map.animateCamera(position);
-      }).catch(err => {
-        this.error = `line 42`;
-      })
-    }
-      this.platform.ready().then(() => {
-        this.error = 'in platform ready';
-        initMap();
-      })
-    }
-}
+// @IonicPage()
+// @Component({
+//   selector: 'page-map',
+//   templateUrl: 'map.html',
+// })
+
+// export class MapPage {
+//   @ViewChild('map') element;
+//   error: any = 'ERROR';
+//   constructor( public navCtrl: NavController, public platform: Platform, public googleMaps: GoogleMaps,) {
+//     const initMap = () => {
+//       this.error = 'in initmap';
+//       let map: GoogleMap = googleMaps.create(this.element.nativeElement);
+//       map.one(GoogleMapsEvent.MAP_READY).then((data: any) => {
+//         this.error = `line 24`;
+//         let coordinates: LatLng = new LatLng(33.6396965, -84.4304574);
+//         let position = {
+//           target: coordinates,
+//           zoom: 17
+//         }
+//         map.animateCamera(position);
+//       }).catch(err => {
+//         this.error = `line 42`;
+//       })
+//     }
+//       this.platform.ready().then(() => {
+//         this.error = 'in platform ready';
+//         initMap();
+//       })
+//     }
+// }
