79567468717fb27c14ce83284091ec3343e9f2b6
Jmetev1 <jmetevier@gmail.com>
2017-09-02 17:29:44 -0500

    [add] your location on map

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,11 +1,5 @@
-import {
-  GoogleMaps,
-  GoogleMap,
-  GoogleMapsEvent,
-  GoogleMapOptions,
-  CameraPosition,
-  MarkerOptions,
-  Marker
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions,
+  CameraPosition, MarkerOptions,  Marker
  } from '@ionic-native/google-maps';
  import { Component } from "@angular/core/";
  
@@ -76,55 +70,3 @@ import {
 
 
 
-
-
-
-// import { Component, ViewChild, ElementRef } from '@angular/core';
-// import { NavController, NavParams } from 'ionic-angular';
-
-// declare var google;
-
-// @Component({
-//  selector: 'Map-page',
-//  templateUrl: 'map.html'
-// })
-// export class MapPage {
-//  @ViewChild('map') 
-//  mapElement: ElementRef;
-//  map: any;
-
-//  constructor(public navCtrl: NavController) {
-
-//  }
-
-//  ionViewDidLoad(){
-//    console.log('in view did load')
-//    this.loadMap();
-//  }
-//  loadMap(){
-  
-//      let latLng = new google.maps.LatLng(-34.9290, 138.6010);
-  
-//      let mapOptions = {
-//        center: latLng,
-//        zoom: 15,
-//        mapTypeId: google.maps.MapTypeId.ROADMAP
-//      }
-  
-//      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-  
-//    }
-//  }
-//  loadMap(){
-//    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
-
-//    let mapOptions = {
-//      center: latLng,
-//      zoom: 15,
-//      mapTypeId: google.maps.MapTypeId.ROADMAP
-//    }
-
-//    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-
-//  }
-// }
