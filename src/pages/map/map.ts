3e003eb884685c11b2a4f13dd7c0e9b6de9284ea
Jmetev1 <jmetevier@gmail.com>
2017-09-04 11:38:16 -0500

    [before] fixing menu

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,72 +1,69 @@
 import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions,
   CameraPosition, MarkerOptions,  Marker
  } from '@ionic-native/google-maps';
- import { Component } from "@angular/core/";
- 
- import { Platform } from 'ionic-angular';
- @Component({
-   selector: 'Map-page',
-   templateUrl: 'map.html'
- })
- export class MapPage {
-   map: GoogleMap;
-   mapElement: HTMLElement;
-   constructor(private googleMaps: GoogleMaps, public platform: Platform) {
- 
-     // Wait the native plugin is ready.
-     platform.ready().then(() => {
-       this.loadMap();
-     });
-   }
- 
-   // Don't use the ngAfterViewInit(). The native plugin is not ready.
-   //ngAfterViewInit() {
-   // this.loadMap();
-   //}
- 
-  loadMap() {
-     this.mapElement = document.getElementById('map');
- 
-     let mapOptions: GoogleMapOptions = {
-       camera: {
-         target: {
-           lat: 43.0741904,
-           lng: -89.3809802
-         },
-         zoom: 18,
-         tilt: 30
-       }
-     };
- 
-     this.map = this.googleMaps.create(this.mapElement, mapOptions);
- 
-     // Wait the MAP_READY before using any methods.
-     this.map.one(GoogleMapsEvent.MAP_READY)
-       .then(() => {
-         console.log('Map is ready!');
+import { Component } from "@angular/core/";
+import { Geolocation } from '@ionic-native/geolocation';
+import { Platform } from 'ionic-angular';
+import { RequestService } from '../../app/request.service'
 
-         // Now you can use all methods safely.
-         this.map.addMarker({
-             title: 'Ionic',
-             icon: 'blue',
-             animation: 'DROP',
-             position: {
-               lat: 43.0741904,
-               lng: -89.3809802
-             }
-           })
-           .then(marker => {
-             marker.on(GoogleMapsEvent.MARKER_CLICK)
-               .subscribe(() => {
-                 alert('clicked');
-               });
-           });
-       });
-   }
- }
-
-
-
+@Component({
+  selector: 'Map-page',
+  templateUrl: 'map.html'
+})
+export class MapPage {
+  map: GoogleMap;
+  holes: any = [1,1,1]
+  mapElement: HTMLElement;
+  constructor(private googleMaps: GoogleMaps, public platform: Platform,
+  private requestService:RequestService, private geolocation: Geolocation,
+) {
+    platform.ready().then(() => {
+      this.outside();
+    });
+  }
+  outside() {
+    let watch = this.geolocation.watchPosition()
+    watch.subscribe((data) => this.loadMap(data))
+  }
+  loadMap(data) {
+      this.mapElement = document.getElementById('map');
+      let mapOptions: GoogleMapOptions = {
+        camera: {
+          target: {
+            lat: data.coords.latitude,
+            lng: data.coords.longitude
+          },
+          //was 18 before, too close
+          zoom: 12,
+          tilt: 30
+        }
+      };
+    this.map = this.googleMaps.create(this.mapElement, mapOptions);
+    // Wait the MAP_READY before using any methods.
+    this.map.one(GoogleMapsEvent.MAP_READY)
+      .then(() => {
+          this.requestService.getPotholes().then(values => {
+            values.forEach(ph => {
+              this.map.addMarker({
+                title: ph.name,
+                icon: 'blue',
+                animation: 'DROP',
+                position: {
+                  lat: ph.lat,
+                  lng: ph.lng
+                }
+              })
+              // .then(marker => {
+              //   marker.on(GoogleMapsEvent.MARKER_CLICK)
+              //     .subscribe(() => {
+              //       // alert('clicked');
+              //     });
+              // });
+            })
+        })
+      });
+  }
+}
 
 
 
