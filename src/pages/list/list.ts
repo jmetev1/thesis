ba47707e7571b1c005b6abba58876f0d5fef002e
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-09-14 15:21:17 -0500

    [update]pothole message

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -32,25 +32,43 @@ export class ListPage {
           return e
         })
         this.impacts.forEach(impact => {
+          console.log(impact)
           this.requestService.getPotholeById(impact.pothole_id)
             .then(hole => {
               this.nativeGeocoder.reverseGeocode(hole.lat, hole.lng)
-              .then(add => {
-                this.potholes.push({
-                  date: impact.date,
-                  force: impact.force,
-                  name: hole.name,
-                  num: add.subThoroughfare,
-                  street: add.thoroughfare
-                })
+              .then(address => {
+                if(impact.force === '0.1,') {
+                  this.potholes.unshift({
+                    date: impact.date,
+                    force: impact.force,
+                    name: hole.name,
+                    num: address.subThoroughfare,
+                    street: address.thoroughfare,
+                    message:`
+                      You manually added the ${hole.name} at ${address.subThoroughfare} ${address.thoroughfare}
+                      on ${impact.date}. Thanks!`
+                  })
+                } else {
+                  this.potholes.unshift({
+                    date: impact.date,
+                    force: impact.force,
+                    name: hole.name,
+                    num: address.subThoroughfare,
+                    street: address.thoroughfare,
+                    message:`
+                      You hit the ${hole.name} at ${address.subThoroughfare} ${address.thoroughfare}
+                      with a force of ${impact.force} Gs on ${impact.date}`
+                  })
+                }
               })
             })
         })
       })
   }
-
+  // You hit {{pothole.name}} at {{pothole.num}} {{pothole.street}} with a force of
+  // {{pothole.force}} Gs on {{pothole.date}}
   postToFb(p: any): any {
-    let message = `I hit the ${p.name} on ${p.date} at ${p.num} ${p.street}. It had a force of ${p.force} g's! @MayorLandrieu`
+    let message = `There's a huge pothole named ${p.name} at ${p.num} ${p.street}! @MayorLandrieu`
     this.socialSharing.share(message, null, null)
   }
 
