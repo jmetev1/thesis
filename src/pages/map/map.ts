6fcf0bd983e18721f6fcfc9df0ea529f29f3124e
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-09-14 19:17:47 -0500

    change coloring

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -49,9 +49,9 @@ export class MapPage {
     };
     this.map = this.googleMaps.create(this.mapElement, mapOptions);
     this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
-      this.map.set('backgroundColor', 'pink');
-      this.requestService.getPotholes().then((values) => {
-        values.forEach((ph) => {
+      this.requestService.getPotholes().then(values => {
+        values.forEach(ph => {
+          // let name = this.bearing(29.945854, -90.070120, ph.lat, ph.lng).toString() + ' ' + this.warner(ph)
           this.map.addMarker({
             title: ph.name,
             icon: 'blue',
