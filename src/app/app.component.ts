67fe53857a9be6a95be4493fc4c46b65343a6186
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-09-15 15:03:19 -0500

    [debug]merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage = ManualEntryPage;
+  rootPage = LoginPage;
   pages: Array<{title: string, component: any}>;
 
   constructor(
